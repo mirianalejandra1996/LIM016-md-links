20d65c903fb6f7514ac3745454cfc6a4809ef55d
Lucero <lucerogoga63@hotmail.com>
2022-02-02 18:32:57 -0500

    creacion funcion filtromd y leerDirectorio

--- api.js
+++ api.js
@@ -1,30 +1,95 @@
+const fs = require("fs");
+const path = require("path");
 
-const fs = require('fs')
-const path = require('path') 
-//const fetch = require('node-fetch'); 
+const rutaExiste = (route) => {
+  return fs.existsSync(route);
+};
 
+const rutaAbsoluta = (route) => {
+  return path.isAbsolute(route);
+};
 
+const convertirAbsoluta = (route) => {
+  return path.resolve(__dirname, route);
+};
 
-// fs.readFile(archivo, () => {
+const isFile = (route) => fs.statSync(route).isFile();
+const isDirectory = (route) => fs.statSync(route).isDirectory();
+// const readDirec = (route) => fs.readdir(route, function (err, archivos) {
+//     if (err) {
+//     onError(err);
+//     return archivos;
+//     }
+//     console.log(archivos);
+//     });
 
-// })
-const rutaAbsoluta = (ar) => {return path.resolve(__dirname, ar) }
+const listArchivosMD = (route, cb) => {
+  let esArchivo = isFile(route);
+  if (esArchivo) {
+    let archivo = [path.basename(route)];
+    console.log(archivo);
+    archivoMd = filtrarMd(archivo);
+    if (archivoMd.length == 0) {
+        cb(new Error("no es un archivo MD"), null)
+    } else {
+     cb(null, [route])
+    }
+  } else {
+    readDirectory(route)
+    .then(archivosMD => {  
+        if (archivosMD.length == 0)
+           cb(new Error("no hay archivos en este directorio"), null)
+        else cb(null, archivosMD)
+    })
+    .catch(err => {
+        cb(err, null)
+    })
+  }
+};
 
-const dirExist = () => {}
+const readDirPromise = (route) =>
+  new Promise((resolve, reject) => {
+    fs.readdir(route, (err, archivos) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(archivos);
+      }
+    });
+  });
 
-const isFile = () => {}
+const readDirectory = async (route) => {
+  const items = await readDirPromise(route);
+  const itemsConPath = items.map((archivoMd) => path.resolve(route, archivoMd));
 
-const listFiles = (nr) => {
-fs.readFile(nr, () => {
-console.log(nr)
-})
-}
+  const archivosMD = filtrarMd(itemsConPath);
 
-const mdlinks = () => {
-   // return console.log("hola");
-}
+  const folders = itemsConPath.filter(isDirectory);
+
+  if (folders.length > 0) {
+    const promises = folders.map((folder) => readDirectory(folder));
+    let archivosEnArrays = await Promise.all(promises);
+    let archivosAplanados = archivosEnArrays.flat();
+
+    archivosMD.push(...archivosAplanados);
+  }
+
+  return archivosMD;
+};
+
+// Filtra archivos md
+const filtrarMd = (archivos) => {
+  return archivos.filter((archivo) => {
+    return path.extname(archivo) == ".md";
+  });
+};
+
+// const readFile = fs.readFileSync(file,'utf-8')
+
 
 module.exports = {
-    rutaAbsoluta,
-    listFiles
+  rutaExiste,
+  rutaAbsoluta,
+  convertirAbsoluta,
+  listArchivosMD,
 };
--- index.js
+++ index.js
@@ -1,16 +1,42 @@
+const md = require("./api.js");
 
-const md = require('./api.js');
-
-let archivo = 'Prueba/directorio'
+let path = "Prueba";
+// let directorio = 'Prueba/directorio'
 // /Users/lucero/Projectos/LIM016-md-links/Prueba
 
-nr = md.rutaAbsoluta(archivo)
-md.listFiles(nr)
 
+if (md.rutaExiste(path)) {
+  absolutePath = md.convertirAbsoluta(path);
+  console.log(absolutePath, "es una ruta absoluta");
+  md.listArchivosMD(path, (err, archivosMD)=> {
+      if(err) {
+          console.log(err)
+      } else {
+          console.log(archivosMD)
+      }
+  })
+//   respuesta = md.isFile(absolutePath);
+//  if (respuesta) {
+//     console.log("es archivo");
+    
+//   } else {
+//     console.log("es directorio");
+//     md.readDirec(absolutePath, (err, archivosMd) => {
+//       if (err) console.log('no tiene archivos');
+//       else if (archivosMd.length == 0) console.log("no hay archivos en este directorio")
+//       else console.log("tiene archivos", archivosMd);
+//     });
+//   } 
+} else {
+  console.log("la ruta no existe");
+}
 
+// const mdlinks = () => {
+//     return new Promise ((res,rej) => {
+//         aqui va el codigo
+//     })
+// };
 
-//md.traerArchivos(nuevaruta)
-
-module.exports = () => {
-
-};
+// module.exports = {
+// mdlinks
+// };
