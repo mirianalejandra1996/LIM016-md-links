732ba05ed3c7b4c6af8c86e62920aa2c153b9712
mirianalejandra1996 <mirianalejandra1996@gmail.com>
2022-02-04 16:03:19 -0500

    limpia test function spec js

--- test/functions.spec.js
+++ test/functions.spec.js
@@ -1,15 +1,11 @@
-// const mdLinks = require('../');
-import {isValidatedPath} from '../src/functions'
+import { isValidatedPath } from "../src/functions";
 
-describe('isValidatePath function', () => {
-
-  it('should return true if path exist', () => {
-    // console.log('FIX ME!');
-    // expect(isValidatedPath('prueba')).toBe(true)
-    const ruta = 'C:\\Users\\Miria\\Desktop\\MD-LINKS\\LIM016-md-links\\src'
-    expect(isValidatedPath(ruta)).toBe(true)
+describe("isValidatePath function", () => {
+  it("should return true if path exist", () => {
+    const ruta = "C:\\Users\\Miria\\Desktop\\MD-LINKS\\LIM016-md-links\\src";
+    expect(isValidatedPath(ruta)).toBe(true);
   });
-
 });
+
 
 // node --experimental-vm-modules node_modules/jest/bin/jest.js --coverage
