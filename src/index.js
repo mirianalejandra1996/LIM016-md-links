13abfdcb7134827e86e1bf9b39f8e4e7bfb6e27d
Lucero <lucerogoga63@hotmail.com>
2022-02-06 10:54:23 -0500

    implementa promesa mdlinks

--- src/index.js
+++ src/index.js
@@ -1,41 +1,28 @@
 const api = require("./api.js");
 
-  let path = "prueba";
+//let path = "prueba";
 // let directorio = 'Prueba/directorio'
 // /Users/lucero/Projectos/LIM016-md-links/Prueba
 
-if (api.rutaExiste(path)) {
-api.listLinks(path).then((links)=>{
- console.log("array de links",links)  
-})
-} else {
-  console.log("la ruta no existe");
-}
+const mdlinks = (path) => {
+    return new Promise ((res,rej) => {
+      if (api.rutaExiste(path)) {
+        if(option == undefined || option.validate == false){
+          api.listLinks(path).then((links)=>{
+          console.log("array de links",links)  
+          res(links)
+         })
+        }else{
+          api.listLinks(path).then((links)=>{
+            linksValidados = api.validatelinks(links)
+            console.log("array de links validados",linksValidados)  
+            res(linksValidados)
+           })
+        }
+      } else {
+        res("la ruta no existe");
+      }
+    })
+};
 
-
-// const mdlinks = (path) => {
-//     return new Promise ((res,rej) => {
-//       if (api.rutaExiste(path)) {
-//         absolutePath = api.convertirAbsoluta(path);
-//         api.listArchivosMD(path, (err, archivosMD)=> {
-//             if(err) {
-//                 console.log(err)
-//             } else {
-//                 archivosMD.forEach(file => {     
-//                 let data =  api.readFiles(file)
-//                 let markDown = api.dataToHtml(data)
-//                 let links = api.getLinks(file,markDown)
-//                 console.log("si validar es false",validacion)
-//                 let validacion = api.validatelinks(links)
-//                 res(validacion)
-//                 console.log("si validar es true",validacion)
-//                 });  
-//             }
-//         })
-//       } else {
-//         console.log("la ruta no existe");
-//       }
-//     })
-// };
-
-// module.exports = mdlinks
+module.exports = mdlinks
