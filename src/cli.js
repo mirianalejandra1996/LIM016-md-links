4db2e81de80d7133f7cb13d2ed97b43a2b8509c3
mirianalejandra1996 <mirianalejandra1996@gmail.com>
2022-02-08 16:47:05 -0500

    practica commander

--- src/cli.js
+++ src/cli.js
@@ -1,55 +1,82 @@
+#!/usr/bin/env node
+
 import { mdLinks } from "./mdlinks.js";
 import { program } from "commander";
 import chalk from "chalk";
+const [, , ...args] = process.argv;
 
 const totalLinks = (links) => links.length;
 const uniqueLinks = (links) => {
   return new Set(links.map((link) => link.href)).size;
 };
-// const brokenLinks = (links) => "broken";
 const brokenLinks = (links) =>
   links.filter((link) => link.statusCode === 404).length;
 
 // mdLinks("../test/Archivos", { validate: true })
-mdLinks("../test/Archivos/filemd2.md", { validate: true })
-  //   .then((res) => console.log(totalLinks(res)))
-  // .then((res) => console.log(uniqueLinks(res)))
-  .then((res) => console.log(brokenLinks(res)))
-  // .then((res) => console.log(res))
-  .catch((err) => console.log(err));
+// mdLinks("../test/Archivos/filemd2.md", { validate: true })
+//   .then((res) => console.log(totalLinks(res)))
+// .then((res) => console.log(uniqueLinks(res)))
+// .then((res) => console.log(brokenLinks(res)))
+// .then((res) => console.log(res))
+// .catch((err) => console.log(err));
 
-// const cli = () => {
+// !-------------------------------------------------------------------------------------------
 
-// }
-
-program.version("No tengo ninguna version");
-
-// console.log(cli);
-// cli.description("Access the JSON placeholder API");
-// cli.name("jsonp");
-
-// Commands
 program
-  .command("say <name>") //<name> es opcional el nombre, puede ser <loQueSea>
-  .description("Say Something Interesting")
-  .alias("s")
-  .action(function (ho) {
-    // console.log("comando => ", ho);
-    // console.log(saludo(ho));
-    console.log(chalk.bgMagentaBright(ho));
-    console.log("probando , ", process.argv.slice(2));
-    // console.log("probando , ", process.argv);
+  .name("string-util")
+  .description("CLI to some JavaScript string utilities")
+  .version("0.8.0");
+
+// program
+//   .command("split")
+//   .description("Split a string into substrings and display as an array.")
+//   .argument("<string>", "string to split")
+//   .option("--first", "display just the first substring")
+//   .option("-s, --separator <char>", "separator character", ",")
+//   .action((str, options) => {
+//     const limit = options.first ? 1 : undefined;
+//     console.log(str.split(options.separator, limit));
+//   });
+
+// program
+//   .command("join")
+//   .description("Join the command-arguments into a single string")
+//   .argument("<strings...>", "one or more strings")
+//   .option("-s, --separator <char>", "separator character", ",")
+//   .action((strings, options) => {
+//     console.log(strings.join(options.separator));
+//   });
+
+// program
+//   .command("--join")
+//   .description("Join the command-arguments into a single string")
+//   .argument("<strings...>", "one or more strings")
+//   .option("-s, --separator <char>", "separator character", ",")
+//   .action((strings, options) => {
+//     console.log(strings.join(options.separator));
+//   });
+// program.parse();
+
+
+
+// !-------------------------------------------------------------------------------------------
+
+program
+  .argument('<name>')
+  .option('-t, --title <honorific>', 'title to use before name')
+  .option('-d, --debug', 'display some debugging')
+  .action((name, options, command) => {
+    if (options.debug) {
+      console.error('Called %s with options %o', command.name(), options);
+    }
+    const title = options.title ? `${options.title} ` : '';
+    console.log(`Thank-you ${title}${name}`);
   });
 
-// .action(function (name) {
-//   console.log("el name es ", name);
-//   // console.log(program);
-//   // console.log(program.name);
-// });
-
-// Options
-// program.option("-d, --do", "Do something").action(function (task) {
-//   console.log("Estoy cansada, ayudenme, pd: Patita Zurda.", task);
-// });
-
 program.parse(process.argv);
+
+
+// !-------------------------------------------------------------------------------------------
+// md-links ./some/example.md --validate
+// md-links ./some/example.md --stats
+// md-links ./some/example.md --stats --validate
