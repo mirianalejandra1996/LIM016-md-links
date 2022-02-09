41d5cb1887caed2f47d8c7f45baf80d7139b027b
Lucero <lucerogoga63@hotmail.com>
2022-02-09 11:53:10 -0500

    envio de los links de la promesa

--- src/index.js
+++ src/index.js
@@ -4,25 +4,25 @@ const api = require("./api.js");
 // let directorio = 'Prueba/directorio'
 // /Users/lucero/Projectos/LIM016-md-links/Prueba
 
-const mdlinks = (path,option) => {
-    return new Promise ((res,rej) => {
-      if (api.rutaExiste(path)) {
-        if(option == undefined || option.validate == false){
-          api.listLinks(path).then((links)=>{
-          console.log("array de links",links)  
+const mdlinks = (path, option) => {
+  return new Promise((res, rej) => {
+    if (api.rutaExiste(path)) {
+      if (option == undefined || option.validate == false) {
+        api.listLinks(path).then((links) => {
           res(links)
-         })
-        }else{
-          api.listLinks(path).then((links)=>{
-            linksValidados = api.validatelinks(links)
-            console.log("array de links validados",linksValidados)  
-            res(linksValidados)
-           })
-        }
+        })
       } else {
-        res("la ruta no existe");
+        api.listLinks(path).then((links) => {
+          api.validatelinks(links)
+            .then((linksValidados) => {
+              res(linksValidados)
+            })
+        })
       }
-    })
+    } else {
+      res("la ruta no existe");
+    }
+  })
 };
 
 module.exports = mdlinks
