93e2c52bd5d5ff7900a95d3a9a14b9945a8d98c7
Lucero <lucerogoga63@hotmail.com>
2022-02-09 11:55:35 -0500

    desarrollo de cli con yargs

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,64 @@
 #!/usr/bin/env node
+
 const mdlinks = require('./index')
+const stats = require('./stats.js')
+const yargs = require('yargs')
+const msg = require('./mensajes.js')
+// const {
+//   hideBin
+// } = require('yargs/helpers')
+// const argv = yargs(hideBin(process.argv)).argv
 
-let path = process.argv[2]
 
-//let option = process.argv[3]
+const argv =
 
-console.log({path})
+  yargs
+  
+  .options({
+    validate: {
+      //   type: "boolean",
+      alias: "v",
+      name: 'validate',
+      describe: 'valida los links'
+    },
+    stats: {
+      //   type: "boolean",
+      alias: "s"
+    }
+})
+.strictOptions(true)
+.showHelpOnFail(false, msg.helpMDLinks())
+.argv
 
-mdlinks(path,{validate:false})
+ let path = (argv._)
+
+// console.log(path[0])
+
+if(path.length > 1){
+console.log("porfavor solo ingrese una ruta")
+}else{
+    if (argv.validate && argv.stats) {
+        mdlinks(path[0],{validate:true}).then((data) => {
+        const total = stats.totalLinks(data)
+        const unique = stats.unique(data)
+        const broken = stats.broken(data)
+        msg.statsAndValidate(total,unique,broken)
+        })
+    } else if (argv.stats) {
+        mdlinks(path[0],{validate:true}).then((data) => {
+        const total = stats.totalLinks(data)
+        const unique = stats.unique(data)
+        msg.stats(total,unique)
+        })
+    } else if (argv.validate) {
+        mdlinks(path[0],{validate:true}).then((data) => {
+        msg.mdlinksCompletedValidate(data)
+        })
+    } else {
+        mdlinks(path[0],{validate:false}).then((data) => {
+            msg.mdlinksCompleted(data)
+        })
+    }
+}
+
+
