8eaeb06e9138f78d7425ff939b5835c680ac84df
mirianalejandra1996 <mirianalejandra1996@gmail.com>
2022-02-15 22:07:10 -0500

    modifica package.json

--- src/mdlinks.js
+++ src/mdlinks.js
@@ -49,24 +49,4 @@ export const mdLinks = (path, options) => {
   });
 };
 
-// ! -----------------------------------------------------------------------------------------
-// C:\Users\Miria\Desktop\MD-LINKS\LIM016-md-links\test\Archivos\filejs.js
-// console.log(convertPathAbsolute("../test/Archivos"));
-// // mdLinks("../test/Archivo23s")
-// mdLinks("../test/ArchivosDañanosjdlskjsdlkfjsj", { validate: true })
-// mdLinks("../test/Archivos", { validate: true })
-// mdLinks("../test/Archivos", { validate: true })
-// mdLinks("../test/Archivos/filejs.js", { validate: true })
-//   // mdLinks("../test/Archivos", { validate: false })
-// mdLinks("../test/Archivos/emptymd.md", { validate: true })
-// mdLinks("../test/Archivos/emptyFolder", { validate: true })
-// mdLinks("../test/Archivos/prueba", { validate: true })
-//   // mdLinks("../test/Archivos", { validate: true })
-//   // mdLinks('../test/rutaInexistente', { validate: true })
-// mdLinks("./test/Archivos", { validate: true })
-// mdLinks("./test/Archivos/emptymd.md", { validate: true })
-// mdLinks("./test/Archivos/filemd2.md", { validate: true })
-// mdLinks("./test/Archivos", { validate: false })
-// mdLinks("./test/Archivos", { validate: true })
-//   .then((res) => console.log(res))
-//   .catch((err) => console.log(err));
+
--- test/mdlinks.spec.js
+++ test/mdlinks.spec.js
@@ -6,3 +6,28 @@ describe("mdlinks function", () => {
     expect("").toBe("");
   });
 });
+
+// C:\Users\Miria\Desktop\MD-LINKS\LIM016-md-links\test\mainFolder\filejs.js
+// console.log(convertPathAbsolute("../test/mainFolder"));
+// // mdLinks("../test/Archivo23s")
+// mdLinks("../test/mainFolderDañanosjdlskjsdlkfjsj", { validate: true })
+// mdLinks("../test/mainFolder", { validate: true })
+// mdLinks("../test/mainFolder", { validate: true })
+// mdLinks("../test/mainFolder/filejs.js", { validate: true })
+//   // mdLinks("../test/mainFolder", { validate: false })
+// mdLinks("../test/mainFolder/emptymd.md", { validate: true })
+// mdLinks("../test/mainFolder/emptyFolder", { validate: true })
+// mdLinks("../test/mainFolder/prueba", { validate: true })
+//   // mdLinks("../test/mainFolder", { validate: true })
+//   // mdLinks('../test/rutaInexistente', { validate: true })
+// mdLinks("./test/mainFolder", { validate: true })
+// mdLinks("./test/mainFolder/emptymd.md", { validate: true })
+// mdLinks("./test/mainFolder/filemd2.md", { validate: true })
+// mdLinks("./test/mainFolder", { validate: false })
+// mdLinks("./test/mainFolder", { validate: true })
+//   .then((res) => console.log(res))
+//   .catch((err) => console.log(err));
+
+// mdLinks("./test/mainFolder", { validate: true })
+//   .then((res) => console.log(res))
+//   .catch((err) => console.log(err));
